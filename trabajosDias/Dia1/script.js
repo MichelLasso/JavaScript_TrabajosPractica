2de659f2fc60404a54c98acff367b1c81207debe
Michel <valerie01liz01@gmail.com>
2024-08-20 08:22:32 -0500

    feat :tada: se agregó la funcion sin parametros con retorno

--- trabajosDias/Dia1/script.js
+++ trabajosDias/Dia1/script.js
@@ -1,22 +1,22 @@
 //imprimir año de nacimiento
+
 const año = 2024
 function nacimiento(edad){
     console.log(año-edad);
 }
-
 nacimiento(17)
 
 //Grados celsius a fahrenheit
+
 var celsius=35
 const convercion= function tem(){
     fahrenheit=32+(9*celsius/5)
     console.log(fahrenheit);
-    
 }
-
 convercion()
 
 //Descuento por llegada
+
 let cliente = 20
 const descuento=  function llegada(){
 
@@ -30,25 +30,34 @@ const descuento=  function llegada(){
     }
 
 }
-
 descuento()
 
+//sin parametrso y sin retorno
 function saludo(){
     console.log("Hola");
     
 }
 saludo();
 
-
+//parametros con retorno
 function suma(num1 , num2){
     return num1 + num2;
 }
-
 console.log(suma(15, 20));
 
-
+//parametros sin retorno
 function multi(a,b){
     console.log(a*b);
     
 }
 multi(13, 8)
+
+
+//sin parametros con retorno
+
+function resta(){
+    let a= 10
+    let b= 5
+    return (a-b)
+}
+console.log(resta());
