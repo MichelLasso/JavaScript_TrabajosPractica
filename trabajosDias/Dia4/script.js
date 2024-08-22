c5b53aa849d5855a1aa552fe031d21782852416d
Michel <valerie01li01@gmail.com>
2024-08-22 12:58:13 -0500

    feat :sparkles: Manipulación de datos json en JavaScript (CRUD)

---
+++ trabajosDias/Dia4/script.js
@@ -0,0 +1,226 @@
+let Datos = [
+    {
+        "informacion_personal": [
+            {
+                "nombre": "Juan Pérez",
+                "edad": 30,
+            
+                "direccion": [
+                    {
+                        "calle": "Calle Principal",
+                        "numero": 123,
+                        "ciudad": "Ciudad Ejemplo"
+                    }
+                ],
+                "contacto": [
+                    {
+                        "correo": "juan.perez@example.com",
+                        "telefono": "+123456789"
+                    }
+                ]
+            }
+        ],
+        "historial_educativo": [
+            {
+                "nivel": "Secundaria",
+                "institucion": "Instituto Secundario",
+                "anio_inicio": 2000,
+                "anio_fin": 2005
+            },
+            {
+                "nivel": "Universidad",
+                "institucion": "Universidad Ejemplar",
+                "titulo": "Licenciatura en Ciencias",
+                "anio_inicio": 2006,
+                "anio_fin": 2010
+            }
+        ],
+        "experiencia_laboral": [
+            {
+                "puesto": "Desarrollador de Software",
+                "empresa": "Tech Solutions",
+                "periodo": "2010-2015",
+                "responsabilidades": [
+                    "Desarrollo de aplicaciones web",
+                    "Mantenimiento de bases de datos"
+                ]
+            },
+            {
+                "puesto": "Gerente de Proyectos",
+                "empresa": "Proyectos Avanzados",
+                "periodo": "2016-actualidad",
+                "responsabilidades": [
+                    "Planificación y supervisión de proyectos",
+                    "Coordinación de equipos"
+                ]
+            }
+        ]
+    }
+]
+console.log(Datos);
+
+var opcion = prompt("--MANIPULACIÓN DE DATOS--\n1. Agregar nuevo empleado\n2. Editar Datos\n3. Eliminar datos\n4. Ver Información")
+
+if (opcion == 1) {
+
+    console.clear()
+
+    console.log("--Agregar Nuevo Empleado--\n");
+    console.log("\nInformación Personal\n");
+
+    var nombre = prompt("Nombre: ");
+    var edad = prompt("Edad: ");
+    var dirección = prompt("Calle: ");
+    var numero = prompt("Número de calle: ");
+    var ciudad = prompt("Ciudad: ");
+    var correo = prompt("Correo: ");
+    var telefono = prompt("Número de teléfono: ");
+
+    console.log("\nHistorial Educativo\n");
+
+    var nivel = prompt("Nivel: ");
+    var institucion = prompt("Institucion: ");
+    var anio_inicio = prompt("Año de inicio: ");
+    var anio_fin = prompt("Año de finalización: ");
+    var titulo = prompt("Titulo: ");
+
+    console.log("\nExperiencia Laboral\n");
+
+    var puesto = prompt("Puesto: ");
+    var empresa = prompt("Empresa: ");
+    var periodo = prompt("Periodo: ");
+    var responsabilidades = prompt("Responsabilidaes: ");
+
+    var nuevoEmpleado = {
+
+        "informacion_personal": [
+            {
+                "nombre": nombre,
+                "edad": edad,
+                "direccion": {
+                    "calle": dirección,
+                    "numero": numero,
+                    "ciudad": ciudad
+                },
+                "contacto": {
+                    "correo": correo,
+                    "telefono": telefono
+                }
+            }
+        ],
+        "historial_educativo": [
+            {
+                "nivel": nivel,
+                "institucion": institucion,
+                "anio_inicio": anio_inicio,
+                "anio_fin": anio_fin,
+                "titulo": titulo
+            },
+        ],
+        "experiencia_laboral": [
+            {
+                "puesto": puesto,
+                "empresa": empresa,
+                "periodo": periodo,
+                "responsabilidades": responsabilidades
+            }
+        ]
+    }
+}
+
+Datos.push(nuevoEmpleado)
+
+console.log(Datos);
+
+if (opcion == 2) {
+
+    console.log("--Editar Datos--");
+
+    function actualizarDato(index, nombre, edad, pais) {
+        if (index >= 0 && index < datos.length) {
+            datos[index] = { nombre, edad, pais };
+            console.log("Dato actualizado:", datos[index]);
+        } else {
+            console.log("Índice no válido");
+        }
+    }
+
+}
+if (opcion==3){
+    console.log("\n--Eliminar datos\n");
+
+    var eliminar= prompt("Que deseas eliminar")
+    for (const i of Datos){
+        for(const x in ["informacion_personal"]){
+            delete Datos.edad
+            console.log("edad",x["edad"]);
+            
+        }
+    }
+    
+}
+if (opcion==4){
+    console.clear()
+    console.log("--Mostrar Datos--");
+    var verOpcion= prompt(
+        "1. Ver información Personal\n"+
+        "2. Ver historial educativo\n"+
+        "3. Ver Experiencia laboral\n"
+    )
+
+    if (verOpcion==1){
+        console.clear()
+        console.log("\n--Información Personal--\n");
+        
+        for (const i of Datos){
+            for (const x of i["informacion_personal"]){
+                console.log("Nombre del empleado: ",x["nombre"]);
+                console.log("Edad: ",x["edad"]);
+
+                for (const b of x["direccion"]){
+                    console.log("Calle: ",b["calle"]);
+                    console.log("Numero de Calle: ",b["numero"]);
+                    console.log("Ciudad: ",b["ciudad"]);
+                }
+                
+                for (const c of x["contacto"]){
+                    console.log("Correo: ",c["correo"]);
+                    console.log("telefono: ",c["telefono"]);
+                }
+            }
+        }
+    }
+    if(verOpcion==2){
+        console.clear()
+        console.log("\n--Historial Educattivo--\n");
+        
+        for (const i of Datos){
+            for(const x of i["historial_educativo"]){
+                console.log("Nivel academico: ",x["nivel"]);
+                console.log("Institución: ",x["institucion"]);
+                console.log("Inicio de año: ",x["anio_inicio"]);
+                console.log("Finalización de año: ",x["anio_fin"]);
+                console.log("Titulo: ",x["titulo"]);
+            }
+        }
+    }
+    if (verOpcion==3){
+        console.clear()
+        console.log("\n--Experiencia Laboral--\n");
+        
+        for (const i of Datos){
+            for (const x of i["experiencia_laboral"]){
+                console.log("Puesto: ",x["puesto"]);
+                console.log("Empresa: ",x["empresa"]);
+                console.log("Periodo: ",x["periodo"]);
+                console.log("Responsabilidades: ",x["responsabilidades"]);
+            }
+        }
+    }
+}
+/*{
+    "puesto": puesto,
+    "empresa": empresa,
+    "periodo": periodo,
+    "responsabilidades": responsabilidades
+}*/
