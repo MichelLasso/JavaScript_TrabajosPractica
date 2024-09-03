2f41e31d06fd8da8b2e058e8b3dd3aeea8227d23
Michel <valerie01liz01@gmail.com>
2024-09-03 08:09:44 -0500

    feat :sparkles: Se agregó funcionamiento al js

--- trabajosDias/Dia9/script.js
+++ trabajosDias/Dia9/script.js
@@ -1,17 +1,39 @@
-var input= document.getElementById("enter").value;
-var submit= document.getElementById("uno");
+document.getElementById("enter").addEventListener("input", (realizar)=>{
+    let idEnter= document.getElementById("enter").value;
 
+    let api= "https://pokeapi.co/api/v2/pokemon/"
 
-fetch("https://pokeapi.co/api/v2/pokemon/1")
-.then(res=>  res.json())
-.then(data=>{
-    console.log(data);
-    mostrar(data)
+    function pokeApi() {
+        fetch(api+idEnter)
+        .then(res => res.json())
+        .then(mostrarDatos=>{
+
+            let nameId= documen.getElementById("name");
+            nameId.innerHTML += `
+                ${mostrarDatos.id} - <span>${mostrarDatos.forms[0].name}</span>
+            `;
+
+            let mostrarGif= document.getElementById("poke");
+            mostrarGif.innerHTML += `
+                <audio autoplay src="${mostrarDatos.cries.latest}"></audio>
+                <img src="${mostrarDatos.sprites.other.shawdown.front_default}">
+            `;
+
+            document.getElementById("uno").addEventListener("click", (anterior)=>{
+                var anteriorId= mostrarDatos.id-1;
+                obtenerId= String(anteriorId);
+                pokeApi()
+
+            })
+
+            document.getElementById("dos").addEventListener("click", (siguiente=>{
+                console.log(mostrarDatos);
+
+                var siguienteId = mostrarDatos.id +1;
+                obtenerId = String(siguienteId);
+                pokeApi()
+                
+            }))
+        })
+    }
 })
-
-function mostrar(data) {
-    var imagen= document.getElementById("poke");
-    imagen.innerHTML += `
-        <img src="${data}">
-    `
-}
